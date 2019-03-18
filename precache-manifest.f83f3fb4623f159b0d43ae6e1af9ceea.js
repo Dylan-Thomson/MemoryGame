1bbed788911c24842cf3e11065671888b907a617
Dylan Thomson <djthomson88@gmail.com>
2019-03-18 16:16:22 -0400

    Updates

---
+++ precache-manifest.f83f3fb4623f159b0d43ae6e1af9ceea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  },
+  {
+    "revision": "5e7401cbaf5b3912238b",
+    "url": "/MemoryGame/static/js/main.42abcc79.chunk.js"
+  },
+  {
+    "revision": "f36054a2f6c5bf348fa1",
+    "url": "/MemoryGame/static/js/2.4214afab.chunk.js"
+  },
+  {
+    "revision": "5e7401cbaf5b3912238b",
+    "url": "/MemoryGame/static/css/main.9da00e19.chunk.css"
+  },
+  {
+    "revision": "87d871cbdc83d74d0246dfdcd676e242",
+    "url": "/MemoryGame/index.html"
+  }
+];
