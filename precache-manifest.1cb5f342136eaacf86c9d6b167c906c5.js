a7d218f36657c4a1a8c1736f3c53a54c65ba8c4b
Dylan Thomson <djthomson88@gmail.com>
2019-03-18 16:49:12 -0400

    Updates

---
+++ precache-manifest.1cb5f342136eaacf86c9d6b167c906c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  },
+  {
+    "revision": "570b5f7f0b2d918e7990",
+    "url": "/MemoryGame/static/js/main.ec0aefe4.chunk.js"
+  },
+  {
+    "revision": "77aa1b75a5ff38259c88",
+    "url": "/MemoryGame/static/js/2.fa6b5c21.chunk.js"
+  },
+  {
+    "revision": "570b5f7f0b2d918e7990",
+    "url": "/MemoryGame/static/css/main.9da00e19.chunk.css"
+  },
+  {
+    "revision": "189b50df3bd09b1d0d9a672266959db1",
+    "url": "/MemoryGame/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MemoryGame/precache-manifest.f83f3fb4623f159b0d43ae6e1af9ceea.js"
+  "/MemoryGame/precache-manifest.1cb5f342136eaacf86c9d6b167c906c5.js"
 );
 
 workbox.clientsClaim();
